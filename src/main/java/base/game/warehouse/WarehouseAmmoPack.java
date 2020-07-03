be7cbd73b1f128ad764c6acff283a07c9090e882
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-07-03 23:48:46 +0200

    reduce cost of normal weapon packas

--- src/main/java/base/game/warehouse/WarehouseAmmoPack.java
+++ src/main/java/base/game/warehouse/WarehouseAmmoPack.java
@@ -1,9 +1,9 @@
 package base.game.warehouse;
 
 public enum WarehouseAmmoPack {
-  A_A_NORMAL(2, 10),
+  A_A_NORMAL(1, 10),
   A_A_PRO(4, 10),
-  A_G_NORMAL(2, 100),
+  A_G_NORMAL(1, 100),
   A_G_PRO(4, 10),
   ;
 
